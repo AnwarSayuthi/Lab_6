ca2f235daa2bcdf264936ef6991d45a7d22d480e
Khairul Anwar bin Ahmad Sayuthi <anwarsayuthi@gmail.com>
2024-05-19 16:24:17 +0800

    changes

--- Lab6_q1.php
+++ Lab6_q1.php
@@ -1,18 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <title>Lab 6 Q1</title>
+<title>Lab 6 Q1</title>
 </head>
 <body>
- <?php 
- $name = "Muhammad Asyraf";
- ?>
- <table>
- <tr>
- <td>Name</td>
- <td><?php echo "$name"; ?></td> 
- </tr>
- </table>
- 
+<?php
+$name = "KHAIRUL ANWAR";
+$matric_number = "DI220146";
+$course = "BACHELOR OF INFORMATION TECHNOLOGY";
+$year_of_study = "2BIT";
+$address = "LOT 74, PEKAN PARIT SIKOM, 82100, PONTIAN, JOHOR";
+?>
+
+<table border="1">
+<tr>
+    <td>Name</td>
+    <td><?php echo $name; ?></td>
+</tr>
+<tr>
+    <td>Matric Number</td>
+    <td><?php echo $matric_number; ?></td>
+</tr>
+<tr>
+    <td>Course</td>
+    <td><?php echo $course; ?></td>
+</tr>
+<tr>
+    <td>Year of Study</td>
+    <td><?php echo $year_of_study; ?></td>
+</tr>
+<tr>
+    <td>Address</td>
+    <td><?php echo $address; ?></td>
+</tr>
+</table>
+
 </body>
 </html>
